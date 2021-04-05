a2e391d95f68c28a6b69f87c08720b3daaf9a717
andochiwa <a1066079469@gmail.com>
2021-04-06 02:07:55 +0900

    update jdk version and LocalDateTime format

--- service/service_edu/src/main/java/com/github/eduservice/entity/EduTeacher.java
+++ service/service_edu/src/main/java/com/github/eduservice/entity/EduTeacher.java
@@ -1,6 +1,7 @@
 package com.github.eduservice.entity;
 
 import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -52,10 +53,12 @@ public class EduTeacher implements Serializable {
 
     @ApiModelProperty(value = "创建时间")
     @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+9")
     private LocalDateTime gmtCreate;
 
     @ApiModelProperty(value = "更新时间")
     @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+9")
     private LocalDateTime gmtModified;
 
 
