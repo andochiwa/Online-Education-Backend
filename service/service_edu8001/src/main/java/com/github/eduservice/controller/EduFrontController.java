b935e535f15ad22df7d8166b2390cac7d5640051
andochiwa <a1066079469@gmail.com>
2021-04-13 22:47:51 +0900

    add front interface

---
+++ service/service_edu8001/src/main/java/com/github/eduservice/controller/EduFrontController.java
@@ -0,0 +1,54 @@
+package com.github.eduservice.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.eduservice.entity.EduCourse;
+import com.github.eduservice.entity.EduTeacher;
+import com.github.eduservice.service.EduCourseService;
+import com.github.eduservice.service.EduTeacherService;
+import com.github.utils.ResultCommon;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author HAN
+ * @version 1.0
+ * @create 2021/4/13
+ */
+@RestController
+@RequestMapping("/eduservice/index")
+@Api("前台页面查询功能")
+public class EduFrontController {
+
+    @Autowired
+    private EduCourseService eduCourseService;
+
+    @Autowired
+    private EduTeacherService eduTeacherService;
+
+    /**
+     * 查询前8条热门课程，前4个热门教师
+     */
+    @GetMapping
+    @ApiOperation("查询前8条热门课程，前4个热门教师")
+    public ResultCommon getTeacherCourse() {
+        // 查询前8条热门课程
+        QueryWrapper<EduCourse> wrapperCourse = new QueryWrapper<>();
+        wrapperCourse.orderByDesc("view_count")
+                .last("limit 8");
+        List<EduCourse> eduCourses = eduCourseService.list(wrapperCourse);
+
+        // 查询前4个热门教师
+        QueryWrapper<EduTeacher> wrapperTeacher = new QueryWrapper<>();
+        wrapperTeacher.orderByDesc("sort")
+                .last("limit 4");
+        List<EduTeacher> eduTeachers = eduTeacherService.list(wrapperTeacher);
+
+        return ResultCommon.success().setData("courses", eduCourses).setData("teachers", eduTeachers);
+    }
+}
