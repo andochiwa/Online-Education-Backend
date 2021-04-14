55aace874d88b210d99f47f3c292b45d2db4055f
andochiwa <a1066079469@gmail.com>
2021-04-15 04:33:40 +0900

    msm module

---
+++ service/service_msm8005/src/main/java/com/github/msm/controller/MsmController.java
@@ -0,0 +1,16 @@
+package com.github.msm.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author HAN
+ * @version 1.0
+ * @create 2021/4/15
+ */
+@RestController
+@RequestMapping("/edu-msm")
+@CrossOrigin
+public class MsmController {
+}
