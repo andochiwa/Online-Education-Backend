7c76ceff495f8b86194a2ec6f5d295f4212d38b1
andochiwa <a1066079469@gmail.com>
2021-04-15 18:20:37 +0900

    user login and return token string

--- service/service_center8005/src/main/java/com/github/center/controller/UcenterMemberController.java
+++ service/service_center8005/src/main/java/com/github/center/controller/UcenterMemberController.java
@@ -1,9 +1,13 @@
 package com.github.center.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.github.center.entity.UcenterMember;
+import com.github.center.service.UcenterMemberService;
+import com.github.utils.ResultCommon;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,8 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-04-15
  */
 @RestController
-@RequestMapping("/center/ucenter-member")
+@RequestMapping("/edu-center")
+@CrossOrigin
+@Api("登陆注册功能")
 public class UcenterMemberController {
+
+    @Autowired
+    private UcenterMemberService ucenterMemberService;
+
+    /**
+     * 前台登录
+     * @param ucenterMember 登录信息
+     */
+    @PostMapping("login")
+    @ApiOperation("前台登录")
+    public ResultCommon loginUser(@RequestBody UcenterMember ucenterMember) {
+        // 获取生成token值
+        String token = ucenterMemberService.login(ucenterMember);
+
+        return ResultCommon.success().setData("token", token);
+    }
+
+
+    // 注册
 
 }
 
--- service/service_center8005/src/main/java/com/github/center/service/UcenterMemberService.java
+++ service/service_center8005/src/main/java/com/github/center/service/UcenterMemberService.java
@@ -1,9 +1,15 @@
 package com.github.center.service;
 
+import cn.hutool.crypto.SecureUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.center.entity.UcenterMember;
 import com.github.center.mapper.UcenterMemberMapper;
+import com.github.utils.JwtUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.Objects;
 
 /**
  * <p>
@@ -16,4 +22,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class UcenterMemberService extends ServiceImpl<UcenterMemberMapper, UcenterMember> {
 
+    /**
+     * 登录
+     * @param member 登录信息
+     * @return token值
+     */
+    public String login(UcenterMember member) {
+
+        String password = member.getPassword();
+        password = SecureUtil.md5(password);
+        String email = member.getEmail();
+
+        // 判断邮箱和密码是否为空
+        if (ObjectUtils.isEmpty(password) || ObjectUtils.isEmpty(email)) {
+            throw new RuntimeException("邮箱或密码不能为空");
+        }
+
+        // 根据用户名查询数据库
+        QueryWrapper<UcenterMember> wrapper = new QueryWrapper<>();
+        wrapper.eq("email", member.getEmail());
+        UcenterMember ucenterMember = super.getOne(wrapper);
+        // 判断用户是否存在以及密码是否正确
+        if (ObjectUtils.isEmpty(ucenterMember) || !Objects.equals(ucenterMember.getPassword(), password)) {
+            throw new RuntimeException("邮箱或密码不正确");
+        }
+
+        // 判断用户是否被禁用
+        if (ucenterMember.getIsDisabled()) {
+            throw new RuntimeException("用户被禁用");
+        }
+
+        return JwtUtils.getJwtToken(ucenterMember.getId(), ucenterMember.getNickname());
+    }
 }
