dedb0d857471b10f12a9e011701ddd11aa151844
andochiwa <a1066079469@gmail.com>
2021-04-20 22:10:05 +0900

    statistic schedule task

--- service/service_statistic8007/src/main/java/com/github/stat/StatisticMain8007.java
+++ service/service_statistic8007/src/main/java/com/github/stat/StatisticMain8007.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author HAN
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.github")
 @EnableFeignClients
+@EnableScheduling
 public class StatisticMain8007 {
 
     public static void main(String[] args){
---
+++ service/service_statistic8007/src/main/java/com/github/stat/schedule/StatCountSchedule.java
@@ -0,0 +1,25 @@
+package com.github.stat.schedule;
+
+import com.github.stat.service.StatisticsDailyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+/**
+ * @author HAN
+ * @version 1.0
+ * @create 04-20-22:00
+ */
+@Component
+public class StatCountSchedule {
+
+    @Autowired
+    private StatisticsDailyService statisticsDailyService;
+
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void count() {
+        statisticsDailyService.countRegister(LocalDate.now().plusDays(-1L).toString());
+    }
+}
